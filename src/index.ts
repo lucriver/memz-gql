b87453d7c759f62c38ef82f8e84caef95f573591
lucriver <lucrivDEV@gmail.com>
2022-01-22 19:52:05 -0600

    v1.0

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloServerPluginLandingPageDisabled } = require("apollo-server-core");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+const MemzAPI = require("./dataSource");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+const port = process.env.PORT || 8080;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    MemzAPI: new MemzAPI(),
+  }),
+  context: ({ req }: any) => {
+    return {
+      token: req.headers.authorization || " ",
+    };
+  },
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port: ${port}.`);
+});
